482b566b64f8ef992c9222b27abaebed876d8591
Carlos Castillo <carloscastillo090916@gmail.com>
2024-08-09 17:11:40 -0500

    style(reminder): :lipstick: Update reminder styles

--- src/modules/reminder/components/reminder-calendar-month-body/components/day/day.tsx
+++ src/modules/reminder/components/reminder-calendar-month-body/components/day/day.tsx
@@ -1,26 +1,52 @@
+import { type MouseEvent } from "react";
 import { cn } from "~/modules/core/lib/utils";
+import { useCreateReminderModal } from "~/modules/reminder/hooks/useCreateReminderModal";
 import { checkIsToday } from "~/modules/reminder/utils/calendar";
+import { AddReminderModal } from "../../../add-reminder-modal";
 import { type DayProps } from "./types";
 
 const Day = ({ day, selectedMonth, rows, ...props }: DayProps) => {
+  const { state, functions } = useCreateReminderModal();
+
+  const onSelectDay = (event: MouseEvent<HTMLDivElement>) => {
+    const element = event.currentTarget;
+    const rect = element.getBoundingClientRect();
+
+    functions.setIsOpenModal(true);
+    functions.adjustModalPosition({ top: rect.bottom, left: rect.left });
+  };
+
   return (
-    <div
-      className="flex h-full w-full flex-col border border-r-primary-light p-2"
-      {...props}
-    >
-      <p
-        className={cn(
-          checkIsToday(day.date)
-            ? "underline decoration-primary"
-            : selectedMonth.monthIndex !== day.monthIndex
-              ? "text-secondary opacity-70"
-              : "text-secondary opacity-100",
-          "text-xs font-medium",
-        )}
+    <>
+      <div
+        className="flex h-full w-full flex-col border border-r-primary-light p-2"
+        onClick={onSelectDay}
+        {...props}
       >
-        {day.dayNumber}
-      </p>
-    </div>
+        <p
+          className={cn(
+            checkIsToday(day.date)
+              ? "underline decoration-primary"
+              : selectedMonth.monthIndex !== day.monthIndex
+                ? "text-secondary opacity-70"
+                : "text-secondary opacity-100",
+            "text-xs font-medium",
+          )}
+        >
+          {day.dayNumber}
+        </p>
+      </div>
+      <AddReminderModal
+        modalRef={state.modalRef}
+        isOpen={state.isOpenModal}
+        setIsOpen={functions.setIsOpenModal}
+        // style={{
+        //   position: "absolute",
+        //   top: state.modalPosition?.top,
+        //   left: state.modalPosition?.left,
+        // }}
+      />
+    </>
   );
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -52,6 +52,10 @@ const config = {
           "100%": { transform: "translateX(0)", opacity: "1" },
         },
       },
+      boxShadow: {
+        modal:
+          "0 24px 38px 3px rgba(0, 0, 0, .14), 0 9px 46px 8px rgba(0, 0, 0, .12), 0 11px 15px -7px rgba(0, 0, 0, .2)",
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
